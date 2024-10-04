beb51890e76af49de2fd31b8738d7f4615366db9
derdevanisbp <derdevanisbp@vcu.edu>
2024-10-03 20:21:55 -0400

    test

--- Real project/Assets/scripts/KillZone.cs
+++ Real project/Assets/scripts/KillZone.cs
@@ -1,18 +1,23 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class KillZone : MonoBehaviour
+public class Killzone : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        
+        if (collision.CompareTag("Player")) // Ensure your player has the "Player" tag
+        {
+            // Call a method to handle player death
+            PlayerDeath(collision.gameObject);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void PlayerDeath(GameObject player)
     {
-        
+        // Implement what happens when the player dies
+        // For example, you can reload the level or respawn the player
+        Debug.Log("Player has entered the killzone!");
+
+        // Example: Reload the current scene
+        UnityEngine.SceneManagement.SceneManager.LoadScene(UnityEngine.SceneManagement.SceneManager.GetActiveScene().name);
     }
 }
